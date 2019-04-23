8f1c2d85278114c7286ea6d8797266c0d4e68908
lmmaa <lmmaa@iscte-iul.pt>
2019-04-23 21:01:42 +0100

    DataBase data read and presentation.

--- ES_SID_2018_LOG_IN_FRONTEND/src/main/java/hello/MainController.java
+++ ES_SID_2018_LOG_IN_FRONTEND/src/main/java/hello/MainController.java
@@ -1,12 +1,20 @@
 package hello;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//@RequestMapping(path = "/index")
 public class MainController {
+	@Autowired
+	private UserRepository userRepository;
 
 	@GetMapping("/login")
 	public String logIn(Model model) {
@@ -16,7 +24,12 @@ public class MainController {
 
 	@PostMapping("/result")
 	public String greetingSubmit(User user, Model model) {
+		model.addAttribute("user", getUser(user).get());
 		return "result";
 	}
 
+	public @ResponseBody Optional<User> getUser(User user) {
+		// This returns a JSON or XML with the users
+		return userRepository.findById(user.getEmail());
+	}
 }
--- ES_SID_2018_LOG_IN_FRONTEND/src/main/java/hello/User.java
+++ ES_SID_2018_LOG_IN_FRONTEND/src/main/java/hello/User.java
@@ -1,24 +1,49 @@
 package hello;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "users")
 public class User {
 
-	private String username;
-	private String password;
+	private int user_id;
+	@Id
+	private String email;
+	private String name;
+	private String professional_category;
 
-	public String getUsername() {
-		return username;
+	public int getId() {
+		return user_id;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setId(int user_id) {
+		this.user_id = user_id;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getName() {
+		return name;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProfessional_category() {
+		return professional_category;
+	}
+
+	public void setProfessional_category(String professional_category) {
+		this.professional_category = professional_category;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
