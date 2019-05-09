662f89aeeefd1d29808d8ac5aff58a8e7fe0ecd9
lmmaa <lmmaa@iscte-iul.pt>
2019-05-09 22:16:36 +0100

    reorganização do projeto

--- ES_SID_2018_LOG_IN_FRONTEND/src/main/java/utilities/StoredProceduresService.java
+++ ES_SID_2018_LOG_IN_FRONTEND/src/main/java/utilities/StoredProceduresService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Service;
 
+@Service("userService")
 public class StoredProceduresService {
 
 	@Autowired
