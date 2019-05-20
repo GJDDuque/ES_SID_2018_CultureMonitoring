78c167830565fe6005aefae581b8d0c160a98d09
GJDDuque <goncalo.duque9@gmail.com>
2019-05-20 21:38:11 +0100

    Páginas de gráficos separadas
    plz no deztroy

--- ES_SID_2018_LOG_IN_FRONTEND/src/main/java/cultura/MeasureController.java
+++ ES_SID_2018_LOG_IN_FRONTEND/src/main/java/cultura/MeasureController.java
@@ -1,23 +1,37 @@
 package cultura;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import cultura.measure.MeasureForm;
 import cultura.measure.MeasureServiceImpl;
+import culture.cultures.Culture;
+import culture.cultures.CultureServiceImpl;
 
 @Controller
 public class MeasureController {
 	@Autowired
 	private MeasureServiceImpl measureServiceImpl;
+	
+	private Culture culture;
+	
+	private CultureServiceImpl cultureServiceImpl = new CultureServiceImpl();
 
-	@GetMapping("/addMeasure")
-	public String addmeasure(Model model, @ModelAttribute String userEmail) {
+	@GetMapping("/addMeasure/{userEmail}")
+	public String addmeasure(Model model, @PathVariable String userEmail) {
+		
+		List<String> cultures = cultureServiceImpl.findByResponsible(userEmail);
+		
+		model.addAttribute("cultures", cultures);
+		
 		model.addAttribute("userEmail", userEmail);
 		model.addAttribute("measureForm", new MeasureForm());
 		return "addMeasure";
@@ -27,7 +41,7 @@ public class MeasureController {
 	public String addmeasure(@ModelAttribute("measureForm") MeasureForm measureForm, BindingResult bindingResult,
 			Model model) {
 		measureServiceImpl.saveMeasure(measureForm);
-		return "redirect:/addMeasure";
+		return "redirect:/welcome";
 
 	}
 
--- ES_SID_2018_LOG_IN_FRONTEND/src/main/java/cultura/UserController.java
+++ ES_SID_2018_LOG_IN_FRONTEND/src/main/java/cultura/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -77,13 +78,13 @@ public class UserController {
 
 	}
 	
-	@GetMapping("/welcome")
-	public String success(Model model) {
+	@GetMapping("/welcome/{culture}")
+	public String success(Model model,@PathVariable String culture) {
 		if (user != null) {
 			List<String> cultures = cultureServiceImpl.findByResponsible(user.getEmail());
 			
 			model.addAttribute("cultures", cultures);
-			
+			model.addAttribute("cultura", culture);
 			model.addAttribute("userEmail", user.getEmail());
 			model.addAttribute("filters", new Filters());
 			if(user.getProfessional_category().equals("Administrador"))
